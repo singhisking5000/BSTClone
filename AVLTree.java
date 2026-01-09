170a6a9a73202dc34f8e6a5a3b49e4d8e4f1406c
singhisking5000 <73306374+singhisking5000@users.noreply.github.com>
2026-01-09 17:49:08 +0000

    plowerlosdfjwer

--- AVLTree.java
+++ AVLTree.java
@@ -2,35 +2,90 @@ public class AVLTree extends BST
 {
     protected void rotateLeft(Node prev, Node targ) 
     {
-        // this code is buns... time to learn how to rotate...
-        if (targ.right == null)
+        if(prev == null)
         {
-            System.out.println("ILLEGAL ROTATION");
-            return;
-        }
-        Node post = targ.right;
-        targ.right = post.left;
-        post.left = targ;
-        if (prev.right == targ)
-        {
-            prev.right = post;
+            Node post = targ.right;
+            targ.right = post.left;
+            post.left = targ;
+            root = post;
         } else
         {
-            prev.left = post;
+            // this code is buns... time to learn how to rotate...
+            if (targ.right == null)
+            {
+                System.out.println("ILLEGAL ROTATION");
+                return;
+            }
+            Node post = targ.right;
+            targ.right = post.left;
+            post.left = targ;
+            if (prev.right == targ)
+            {
+                prev.right = post;
+            } else
+            {
+                prev.left = post;
+            }
         }
-        
     }
 
     protected void rotateRight(Node prev, Node targ)
     {
-        if (targ.left == null)
+        if (prev == null)
         {
-            System.out.println("ILLEGAL ROTATION");
-            return;
+            Node post = targ.left;
+            targ.left = post.right;
+            post.right = targ;
+            root = post;
+        } else
+        {
+            if (targ.left == null)
+            {
+                System.out.println("ILLEGAL ROTATION");
+                return;
+            }
+            Node post = targ.left;
+            targ.left = post.right;
+            post.right = targ;
+            if (prev.right == targ)
+            {
+                prev.right = post;
+            } else
+            {
+                prev.left = post;
+            }
         }
-        Node post = targ.left;
-        targ.right = post.left;
-        post.left = targ;
-        prev.right = post;
+    }
+
+    // NOW OVERRIDE INSERT SO IT AUTOBALANCES ON INSERTS!
+    protected void insertP(int key, Node curr) {
+            // If its the first is empty
+        if (curr == null) {
+            curr = root = new Node(key);
+        } else if (key < curr.key) // If the val belongs to the left...
+        {
+        if (curr.left == null) // AND the left is empty, put left
+        {
+            curr.left = new Node(key);
+        } else // If not empty, but we need to go that way, let us continue down again until
+        {
+            insertP(key, curr.left);
+        }
+        } else if (key > curr.key) // If val belongs to the right...
+        {
+            if (curr.right == null) // AND the right is empty, put right
+            {
+                curr.right = new Node(key);
+            } else // if not, continue right
+            {
+                insertP(key, curr.right);
+            }
+        }
+
+        System.out.println("At " + curr.key + ". Has a balance of " + balanceCheck(curr)); //<-------- causing a null pter exception through height check
+        if(balanceCheck(curr) > 1)
+        {
+            // WE got problems boys
+        }
     }
 }
--- Main.java
+++ Main.java
@@ -2,20 +2,22 @@ public class Main {
     public static void main(String[] args) {
         AVLTree tree = new AVLTree();
         //
-        tree.insert(10);
-        tree.insert(1);
+        tree.insert(6);
+        tree.insert(5);
+        tree.insert(4);
         tree.insert(2);
         tree.insert(3);
-        tree.insert(4);
+        // tree.insert();
+        // tree.insert();
         // tree.insert(6);
         tree.printTree();
         System.out.println(tree);
         System.out.println("----------------------------------------------------");
-        // TESTING FOR REMOVE VVVVVVVVVVV
         // int num = 5;
-        // System.out.println("removing " + num);
         // tree.remove(num);
-        tree.rotateLeft(tree.root, tree.root.left);
+        tree.rotateLeft(tree.root.left.left, tree.root.left.left.left);
+        tree.rotateRight(tree.root.left, tree.root.left.left);
+        tree.rotateRight(tree.root, tree.root.left);
         tree.printTree();
         System.out.println(tree.toString());
         System.out.println("----------------------------------------------------");
